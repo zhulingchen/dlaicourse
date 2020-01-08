e1a0359b0dcd181841e1488d78b29eb17184bb6a
Lingchen Zhu <zhulingchen@gmail.com>
2020-01-07 17:30:01 -0800

    finished the exercise of TensorFlow Deployment\Course 1 - TensorFlow-JS\Week 4

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
@@ -25,20 +25,20 @@ async function train() {
   // using ReLu activation functions where applicable.
   model = tf.sequential({
     layers: [
-        
-      // YOUR CODE HERE
-
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({ units: 100, activation: 'relu'}),
+      tf.layers.dense({ units: 5, activation: 'softmax'})
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001);
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -71,18 +71,14 @@ function handleButton(elem){
 			spockSamples++;
 			document.getElementById("spocksamples").innerText = "Spock samples:" + spockSamples;
 			break;
-            
-        // Add a case for lizard samples.
-        // HINT: Look at the previous cases.
-            
-        // YOUR CODE HERE
-		
-            
+    case "4":
+      lizardSamples++;
+      document.getElementById("lizardsamples").innerText = "Lizard samples:" + lizardSamples;
+      break; 
 	}
 	label = parseInt(elem.id);
 	const img = webcam.capture();
 	dataset.addExample(mobilenet.predict(img), label);
-
 }
 
 async function predict() {
@@ -108,19 +104,15 @@ async function predict() {
 		case 3:
 			predictionText = "I see Spock";
 			break;
-            
-        // Add a case for lizard samples.
-        // HINT: Look at the previous cases.
-            
-        // YOUR CODE HERE 
-	
-            
-	}
-	document.getElementById("prediction").innerText = predictionText;
-			
-    
-    predictedClass.dispose();
-    await tf.nextFrame();
+    case 4:
+      predictionText = "I see Lizard";
+      break;         
+  }
+  
+  document.getElementById("prediction").innerText = predictionText;
+  
+	predictedClass.dispose();
+  await tf.nextFrame();
   }
 }
 
@@ -142,7 +134,7 @@ function stopPredicting(){
 
 
 function saveModel(){
-    model.save('downloads://my_model');
+  model.save('downloads://my_model');
 }
 
 
@@ -150,7 +142,6 @@ async function init(){
 	await webcam.setup();
 	mobilenet = await loadMobilenet();
 	tf.tidy(() => mobilenet.predict(webcam.capture()));
-		
 }
 
 
